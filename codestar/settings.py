e5b899a67c230512c7e0d94d38fbb5964cce5e67
Saikou Gassama <gassamasaikou@yahoo.com>
2023-08-17 07:54:03 +0000

    connect workspace to database

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 from pathlib import Path
 
@@ -20,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&*=kbiao^ib=7$j=58#rhcedw33ea#i*9%=mub%2@!kzx3#g$'
+# SECRET_KEY = 'django-insecure-5&*=kbiao^ib=7$j=58#rhcedw33ea#i*9%=mub%2@!
+# kzx3#g$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gassama94-djangoblog-nz1uauayuxs.ws-eu103.gitpod.io']
+ALLOWED_HOSTS = ['8000-gassama94-djangoblog-w7m8yepboti.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -74,11 +81,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+# 'default': {
+#   'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
